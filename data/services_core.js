e2b9d46ba962d5056b9ae1fa11c98525cd5d8840
Felix Fung <felix.the.cheshire.cat@gmail.com>
2013-03-02 03:02:11 -0800

    Workaround for Pinning Straight Images
    Shippable!

--- data/services_core.js
+++ data/services_core.js
@@ -250,33 +250,3 @@ if (!pinterestrc.SiteServicesController) {
     };
   })();
 }
-
-self.on("context", function(aTarget) {
-  let doc = aTarget.ownerDocument;
-  let currentLocation = doc.location;
-  let ssc = pinterestrc.SiteServicesController;
-  let pinnable = ssc.handleLocation(currentLocation, aTarget);
-
-  if (pinnable) {
-    if (ssc.lastData.url === undefined) {
-      ssc.lastData.url = doc.location.href;
-    }
-
-    // Not sure what Pinterest uses the title for, but let's give it to them
-    let pageTitle = doc.title;
-    if (pageTitle) {
-      ssc.lastData.title = pageTitle;
-      if (!ssc.lastData.description) {
-        ssc.lastData.description = pageTitle;
-      }
-    }
-
-    self.postMessage({ type : 'label', label : ssc.lastData.label });
-  }
-  return pinnable;
-});
-
-self.on("click", function(aTarget) {
-  pinterestrc.SiteServicesController.lastData.type = 'pin';
-  self.postMessage(pinterestrc.SiteServicesController.lastData);
-});
